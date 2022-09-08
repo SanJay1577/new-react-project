5ac7fd0a9ba30160f2de6d50ea795ffc64ecb4b7
Sanjay R <sanjayprashanth1997@gmail.com>
2022-09-08 21:29:30 +0530

    day-8-9-2020 first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-         New App
-    </div>
+   <div>
+   New React Project
+   </div>
   );
 }
 
 export default App;
+//  Brain stroming for new app
+// terminal codes
+// ##################
+// git clone https://github.com/SanJay1577/new-react-project.git
+// cd new-react-project.git
+// code . 
+// npm start
+// ###########
